05056aa9df9403f60066df225b4169b4cf9295a4
sujan pal <sujanpal79@gmail.com>
2023-05-16 02:42:26 +0530

    Build Botton

--- src/components/ButtonList.js
+++ src/components/ButtonList.js
@@ -1,8 +1,25 @@
 import React from 'react'
+import Button from './Button'
 
+
+const List = ["All", "Gaming", "Songs", "Live", "Soccer", "Cricket", "Cooking", "Movies", "News", "Valentines", "Trailers", "Watched" ]
 const ButtonList = () => {
   return (
-    <div>ButtonList</div>
+    <div className="flex">
+      {/* <Button name="All"/>
+      <Button name="Gaming"/>
+      <Button name="Songs"/>
+      <Button name="Live"/>
+      <Button name="Soccer"/>
+      <Button name="Cricket"/>
+      <Button name="Cooking"/>
+      <Button name="Movies"/>
+      <Button name="News"/>
+      <Button name="Valentines"/> */}
+      {List.map( (list, index) =>{
+        return <Button key={index} name={list}/>
+      })}
+    </div>
   )
 }
 
