cc8066e8150f14097dfa67240f8e483fc227f49c
sujan pal <sujanpal79@gmail.com>
2023-05-22 18:10:32 +0530

    Create comment Data

--- src/components/CommentsContainer.js
+++ src/components/CommentsContainer.js
@@ -1,5 +1,81 @@
 import React from 'react'
 
+const commentsData = [
+  {
+    name: "Sujan Pal",
+    text: "Lorem ipsum dolor sit amet, consecttur adip",
+    replies: [
+
+    ]
+  },
+  {
+    name: "Sujan Pal",
+    text: "Lorem ipsum dolor sit amet, consecttur adip",
+    replies: [
+      {
+        name: "Sujan Pal",
+        text: "Lorem ipsum dolor sit amet, consecttur adip",
+        replies: [
+    
+        ]
+      },
+      {
+        name: "Sujan Pal",
+        text: "Lorem ipsum dolor sit amet, consecttur adip",
+        replies: [
+          {
+            name: "Sujan Pal",
+            text: "Lorem ipsum dolor sit amet, consecttur adip",
+            replies: [
+              {
+                name: "Sujan Pal",
+                text: "Lorem ipsum dolor sit amet, consecttur adip",
+                replies: [
+                  {
+                    name: "Sujan Pal",
+                    text: "Lorem ipsum dolor sit amet, consecttur adip",
+                    replies: [
+                
+                    ]
+                  },
+                  {
+                    name: "Sujan Pal",
+                    text: "Lorem ipsum dolor sit amet, consecttur adip",
+                    replies: [
+                
+                    ]
+                  }
+                ]
+              },
+            ]
+          },
+        ]
+      }
+    ]
+  },
+  {
+    name: "Sujan Pal",
+    text: "Lorem ipsum dolor sit amet, consecttur adip",
+    replies: [
+      
+    ]
+  },
+  {
+    name: "Sujan Pal",
+    text: "Lorem ipsum dolor sit amet, consecttur adip",
+    replies: [
+      
+    ]
+  },
+  {
+    name: "Sujan Pal",
+    text: "Lorem ipsum dolor sit amet, consecttur adip",
+    replies: [
+      
+    ]
+  },
+  
+]
 const CommentsContainer = () => {
   return (
     <div className="m-5 p-2">
