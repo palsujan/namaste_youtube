2358e0b05b30881752c34f1aab2965a07b7d5bd0
sujan pal <sujanpal79@gmail.com>
2023-05-25 22:26:34 +0530

    Updated API Polling

--- src/components/LiveChat.js
+++ src/components/LiveChat.js
@@ -1,9 +1,36 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import ChatMessage from './ChatMessage'
+import { useDispatch, useSelector } from 'react-redux';
+import { addMessage } from '../utils/chatSlice';
+import { generateRandomName, makeRandomMessage } from './helper';
 
 const LiveChat = () => {
+  const dispatch = useDispatch();
+  const chatMessages = useSelector((store) => store.chat.messages)
+
+    useEffect(() =>{
+        const i = setInterval(()=>{
+            //API Polling
+            console.log("Api Polling");
+            dispatch(
+              addMessage({
+              name:generateRandomName() ,
+              message: makeRandomMessage(20),
+            }))
+        },2000);
+        return () => clearInterval(i);
+    },[]);
   return (
-    <div>LiveChat</div>
+      <div className="">
+          <div className="h-[500px] ml-2 p-2 border border-black bg-slate-100 rounded-lg overflow-y-scroll flex flex-col-reverse">
+            {
+              chatMessages.map((chat, index) => (
+              <ChatMessage key={index} name={chat.name} message={chat.message} />
+            ))}
+            
+        </div>
+      </div>
   )
 }
 
-export default LiveChat
+export default LiveChat;
